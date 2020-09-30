0a7c59233282f862be9ce231d2e2fd97b7762248
Sydney Krantz <syd107@gmail.com>
2020-09-29 21:59:59 -0700

    sticky nav and on scroll effects with links and buttons

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,65 +1,64 @@
-// function isInArray(sortedArray, int) {
-//     if (sortedArray.length === 1) {
-//       return int === sortedArray[0];
-      
-//     }
-//     const left = sortedArray.slice(0, Math.ceil(sortedArray.length / 2));
-    
-//     const last = left[left.length - 1];
-    
-//     if (int <= last) {
-//         return isInArray(left, int); 
-//     } else {
-//         const right = sortedArray.slice(Math.ceil(sortedArray.length / 2));
-//         return isInArray(right, int); 
-//     }
-// }
-// console.log(
-//     isInArray([1, 3, 5, 7, 9], 3)
-//     )
+$(document).ready(function () {
 
-/*
-  You are given an array that has 1 rotation in it. 
-  Find and return the lowest integer in this array
-  
-  findLowest([4, 5, 6, 7, 1, 2, 3]) => 1
-  findLowest([12, 13, 14, 15, 2, 3, 8, 11]) => 2
-*/
-function findLowest(array) {
-    if (array.length === 1) {
-      return array[0];
-    }
-    
-    const left = array.slice(0, Math.ceil(array.length / 2));
-    const right = array.slice(Math.ceil(array.length / 2));
-    
-    // If left array and right array both have a length of 1...
-    if (left.length === 1 && right.length === 1) {
-      if (left[0] < right[0]) {
-        return left[0];
+    // for sticky nav 
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
       } else {
-        return right[0];
+        $("nav").removeClass("sticky");
       }
-    } 
-    
-    
-    if (left[0] > left[left.length - 1]) {
-      // We know this array is not sorted... So...
-      return findLowest(left);
-    } else {
-      // The left array is sorted! What do we do now?
-      
-      // if the last value of the "left array" 
-      // is HIGHER than the 
-      // first value of the "right array"
-      
-      if (left[left.length - 1] > right[0]) {
-        // This means the first value in the "right array" is the lowest   
-        return right[0];     
-      }
-      
-  
-      return findLowest(right);
+    },
+    {
+      offset: "60px",
     }
-  }
-  
+  );
+
+//   landing scroll-to buttons
+$('.js--scroll-plans').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+});
+
+$('.js--scroll-start').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+})
+});
+
+// navigation scroll
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
